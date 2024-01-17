cc4e1c7d4d1137517447b36b403adba852423ce1
mkamsani <mkamsani@posteo.org>
2024-01-17 23:36:55 +0800

    doc: add fablo url

--- setup-fablo.sh
+++ setup-fablo.sh
@@ -3,8 +3,9 @@
 
 #
 # This script downloads the latest stable release of fablo.sh
-# fablo.sh is used to generate the configuration files for the
-# Hyperledger Fabric network.
+# https://github.com/hyperledger-labs/fablo/
+#
+# Fablo generates the config files for the Hyperledger Fabric network.
 #
 
 url=https://github.com/hyperledger-labs/fablo/releases/download/1.2.0/fablo.sh
