8d14198773e577138f73cb0edb215d90f743dcee
mkamsani <mkamsani@posteo.org>
2024-01-26 00:06:29 +0800

    chore: deprecate shell script tests

--- test/concurrent_stress.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-for i in $(seq 0 100000); do ./invokeVote_Put.sh; done
--- test/userEnrollAdminPOST.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -sS --request POST --url http://localhost:8801/user/enroll \
- --header 'Authorization: Bearer' \
- --data "{\"id\": \"admin\", \"secret\": \"adminpw\"}"
