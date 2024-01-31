e33dd7b372dc7d3adda2c7b124f3a446955c523b
mkamsani <mkamsani@posteo.org>
2024-01-31 10:49:29 +0800

    feat: dockerize blockchain

--- setup-fablo.sh
+++ setup-fablo.sh
@@ -5,10 +5,46 @@
 # This script downloads the latest stable release of fablo.sh
 # https://github.com/hyperledger-labs/fablo/
 #
+# TODO: Comments on switch case and dockerized build.
+#
 # Fablo generates the config files for the Hyperledger Fabric network.
 #
 
+default () {
 url=https://github.com/hyperledger-labs/fablo/releases/download/1.2.0/fablo.sh
 script=$(basename $url)
 curl -fsSL "$url" -o "$script"
 chmod +x "$script"
+}
+
+fablo_rest () {
+_dockerfile=$(mktemp)
+cat <<EOF > "$_dockerfile"
+FROM softwaremill/fablo-rest:0.1.0
+RUN sed -i.bak 's/app.use(express_1.default.json({ type: () => "json" }))/app.use(express_1.default.json({ type: () => "json", limit: "500mb" }))/g' index.js
+EOF
+printf '%s' "$_dockerfile"
+}
+
+entrypoint () {
+default
+sleep 60
+./fablo.sh recreate
+tail -f /dev/null # Leave it running.
+}
+
+main () {
+test -z "$1" && default && exit 0
+case "$1" in
+  entrypoint)
+    entrypoint
+  ;;
+  fablo_rest)
+    fablo_rest
+  ;;
+  *)
+    default
+  ;;
+esac
+}
+main "$@"
